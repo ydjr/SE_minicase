2f8fd01377fb07a48b53ae57a1e1f6e12c0ae108
Yada Jarasvijitkul <161341113+ydjr@users.noreply.github.com>
2025-02-27 13:51:57 +0700

    Update Main.java

--- HolidayTravelVehicles/Main.java
+++ HolidayTravelVehicles/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Vehicle myVehicle = new Vehicle("001", "Toyota Corolla", "LE", 2023, "Toyota", 1500000.00);
+        Vehicle myVehicle = new Vehicle("001", "Honda Civic", "LE", 2023, "Honda", 1500000.00);
         myVehicle.displayVehicleInfo();
     }
 }
